dca2f2e18d217d424da178f6f82f038cc2b6cb05
kayloud <kloud73@gmail.com>
2018-02-12 16:04:24 -0600

    Added line

--- HelloWorld/HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld/HelloWorld.cs
@@ -7,6 +7,9 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Anohter line");
+
             Console.ReadKey();
         }
     }
